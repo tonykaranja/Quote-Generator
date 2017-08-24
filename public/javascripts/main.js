9575076e7f0fbc801821218a587c2334bc740975
Tony Karanja <fenixpiconi@gmail.com>
2017-08-24 15:28:14 -0700

    functionality of the site works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
-  $("#refresh").click(carouselFiller);
+  carouselFiller();
+
+
+
 
 });
 
@@ -8,30 +11,29 @@ function carouselFiller() {
   $.getJSON("quotes.json", function(data){
     var quoteData = '';
     $.each(data,function (key, value) {
-      quoteData += '<div class="carousel-item">';
+      quoteData += '<div class="quote-item">';
       quoteData += '<p>' + value.quote + '</p>'
       quoteData += '<p>' + value.author + '</p>'
       quoteData += '</div>'
     })
     $('#things').append(quoteData)
-    console.log(quoteData);
   });
 }
 
 var slideIndex = 1;
-showSlides(slideIndex);
+showDivs(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-function showSlides(n) {
+function showDivs(n) {
   var i;
-  var slides = document.getElementsByClassName('mySlides');
-  if (n > slides.length) { slideIndex =  1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+  var x = document.getElementsByClassName("quote-item");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = 'block';
+  x[slideIndex-1].style.display = "block";
 }
